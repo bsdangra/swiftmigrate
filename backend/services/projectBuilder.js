be2525331e5b34a3b385fdbec1db59b7de5e25c8
Seema <31051494+seemamittal89@users.noreply.github.com>
2026-04-27 14:05:16 +0530

    Update projectBuilder.js

--- backend/services/projectBuilder.js
+++ backend/services/projectBuilder.js
@@ -11,9 +11,17 @@ export async function buildProject(convertedFiles) {
   // 2. Create folders
   const pagesDir = path.join(outputDir, "pages");
   const testsDir = path.join(outputDir, "tests");
+  const utilsDir = path.join(outputDir, "utils");
+  const baseDir = path.join(outputDir, "base");
+  const miscDir = path.join(outputDir, "misc");
+
 
   await fs.ensureDir(pagesDir);
   await fs.ensureDir(testsDir);
+  await fs.ensureDir(utilsDir);
+  await fs.ensureDir(baseDir);
+  await fs.ensureDir(miscDir);
+
 
   // 3. Write converted files
   for (const [fileName, data] of Object.entries(convertedFiles)) {
@@ -23,10 +31,24 @@ export async function buildProject(convertedFiles) {
 
     if (data.type === "test") {
       filePath = path.join(testsDir, `${baseName}.spec.ts`);
-    } else {
-      filePath = path.join(pagesDir, `${baseName}.ts`);
-    }
+     data.content = data.content
+    .replace(/(['"`])\.\/pages\//g, '$1../pages/')
+    .replace(/(['"`])\.\/utils\//g, '$1../utils/')
+    .replace(/(['"`])\.\/base\//g, '$1../base/');
+     //content.replace(/(['"`])\.\/pages\//g, '$1../pages/');
+    } else if (data.type === "pageObject") {
+  filePath = path.join(pagesDir, `${baseName}.ts`);
 
+} else if (data.type === "utility") {
+  filePath = path.join(utilsDir, `${baseName}.ts`);
+
+} else if (data.type === "base") {
+  filePath = path.join(baseDir, `${baseName}.ts`);
+
+} else {
+  // fallback (avoid silently putting wrong files in pages)
+  filePath = path.join(miscDir, `${baseName}.ts`);
+}
     await fs.writeFile(filePath, data.content, "utf-8");
   }
 
